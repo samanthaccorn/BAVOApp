c41660cfa7099e785cce3ce1d22dbf3cc130deef
Samantha Corn <samanthaccorn@gmail.com>
2017-04-21 16:04:28 -0400

    Resources Stuff

--- app/src/main/java/com/math497/app/MainActivity.java
+++ app/src/main/java/com/math497/app/MainActivity.java
@@ -139,7 +139,10 @@ public class MainActivity extends AppCompatActivity {
 
     /** Swaps fragments in the main content view */
     private void selectItem(int position) {
-        // Create a new fragment and specify the planet to show based on position
+        //Deals with the user's selection
+
+
+
         /*Fragment fragment = new PlanetFragment();
         Bundle args = new Bundle();
         args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
@@ -149,11 +152,11 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, fragment)
-                .commit();
+                .commit();*/
 
         // Highlight the selected item, update the title, and close the drawer
         mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);*/
+        setTitle(mPageTitles[position]);
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 
